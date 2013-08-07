c2f45226fac36f6ad3d6276f28e6345182ec9a85
Ramunas <ramunas.daukas@gmail.com>
2013-08-07 15:44:12 -0700

    Added index file data reading

--- web.js
+++ web.js
@@ -2,9 +2,18 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+fs = require('fs');
+fs.readFile('/etc/hosts', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  //console.log(data);
+  app.get('/', function(request, response) {                                                         response.Asend(data);
+  });
+ 
 });
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
